a3dfc79e515159dacd66cd7310cc95cf0a8c55b7
Shantanu Sarkar <shantanusarkar867@gmail.com>
2026-02-27 15:26:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,4 +25,48 @@ marker.bindPopup("<b>Pune City</b><br>This is a marker.").openPopup();
 function toggleContribution() {
   const box = document.getElementById('contributionBox');
   box.style.display = box.style.display === 'block' ? 'none' : 'block';
+
+}
+function showMap(){
+  document.getElementById("mapView").style.display="block";
+  document.getElementById("feedView").style.display="none";
+}
+
+function showFeed(){
+  document.getElementById("mapView").style.display="none";
+  document.getElementById("feedView").style.display="block";
+}
+
+function openPostBox(){
+  document.getElementById("postForm").style.display="block";
+}
+function submitPost(){
+  const text=document.getElementById("postText").value;
+  const file=document.getElementById("postMedia").files[0];
+  const container=document.getElementById("feedContainer");
+
+  const card=document.createElement("div");
+  card.className="postCard";
+
+  if(text){
+    card.innerHTML=`<p>${text}</p>`;
+  }
+
+  if(file){
+    const reader=new FileReader();
+    reader.onload=function(e){
+      if(file.type.startsWith("image")){
+        card.innerHTML+=`<img src="${e.target.result}" width="100%">`;
+      }
+      else if(file.type.startsWith("video")){
+        card.innerHTML+=`<video controls width="100%" src="${e.target.result}"></video>`;
+      }
+    };
+    reader.readAsDataURL(file);
+  }
+
+  container.prepend(card);
+
+  document.getElementById("postText").value="";
+  document.getElementById("postMedia").value="";
 }
